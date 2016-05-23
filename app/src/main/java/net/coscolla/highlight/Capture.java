a11b7ba63db86a9fd330de11695a2ec91f74e326
Jordi Coscolla <jordi@coscolla.net>
2016-05-23 23:08:25 +0200

    Capture image

--- app/src/main/java/net/coscolla/highlight/Capture.java
+++ app/src/main/java/net/coscolla/highlight/Capture.java
@@ -1,15 +1,36 @@
 package net.coscolla.highlight;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Capture extends AppCompatActivity {
+
+  static final int REQUEST_IMAGE_CAPTURE = 1;
+  private static final int REQUEST_PERMISSIONS = 2;
+  private ImageView imagePreview;
+  private String mCurrentPhotoPath;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +39,88 @@ public class Capture extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
+    imagePreview = (ImageView) this.findViewById(R.id.image_preview); // TODO Databinding
+
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show());
+    fab.setOnClickListener((e) -> this.takePicture());
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    checkPermissions();
+  }
+
+  private void takePicture() {
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+      File outputFile = null;
+      try {
+        outputFile = createImageFile();
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(outputFile));
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+      setPic();
+    }
+  }
+
+  private File createImageFile() throws IOException {
+    // Create an image file name
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    String imageFileName = "JPEG_" + timeStamp + "_";
+    File storageDir = getExternalFilesDir(null);
+    File image = File.createTempFile(
+        imageFileName,
+        ".jpg",
+        storageDir
+    );
+
+    // Save a file: path for use with ACTION_VIEW intents
+    mCurrentPhotoPath = image.getAbsolutePath();
+    return image;
+  }
+
+
+  private void setPic() {
+    // Get the dimensions of the View
+    int targetW = imagePreview.getWidth();
+    int targetH = imagePreview.getHeight();
+
+    // Get the dimensions of the bitmap
+    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+    bmOptions.inJustDecodeBounds = true;
+    BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+    int photoW = bmOptions.outWidth;
+    int photoH = bmOptions.outHeight;
+
+    // Determine how much to scale down the image
+    int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+    // Decode the image file into a Bitmap sized to fill the View
+    bmOptions.inJustDecodeBounds = false;
+    bmOptions.inSampleSize = scaleFactor;
+    bmOptions.inPurgeable = true;
+
+    Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+    imagePreview.setImageBitmap(bitmap);
+  }
+
+  private void checkPermissions() {
+    int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+    if(permissionCheck != PackageManager.PERMISSION_GRANTED) {
+
+      ActivityCompat.requestPermissions(this,
+          new String[]{Manifest.permission.CAMERA},
+          REQUEST_PERMISSIONS);
+    }
   }
 
   @Override
